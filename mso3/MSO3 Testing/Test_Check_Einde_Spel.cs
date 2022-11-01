f91dbed625ec457e367ddbf7c8bf22de4d72aa2f
Marit <marit.kloeg@gmail.com>
2022-11-01 16:55:34 +0100

    kuiltjes testen erin

--- mso3/MSO3 Testing/Test_Check_Einde_Spel.cs
+++ mso3/MSO3 Testing/Test_Check_Einde_Spel.cs
@@ -6,17 +6,38 @@ namespace MSO3_Testing
 {
     public class Test_Check_Einde_Spel
     {
-        [Fact]
-        public void CheckEinde_True()
+        [Theory]
+        [InlineData(Spel.spelers.p1)]
+        [InlineData(Spel.spelers.p2)]
+        public void check_einde_true(Spel.spelers speler_nummer)
         {
             // Arrange
-            Bord bord = new(6, 0, true);
+            Bord bord = new Bord(6, 0, true);
+            Check_Einde_Spel einde = new Check_Einde_Spel();
+            Speler speler = new Speler(speler_nummer);
+
+            // Act
+            bool is_einde = einde.check_einde(bord, speler);
+
+            // Assert
+            Assert.True(is_einde);
         }
 
-        [Fact]
-        public void CheckEinde_False()
+        [Theory]
+        [InlineData(Spel.spelers.p1)]
+        [InlineData(Spel.spelers.p2)]
+        public void check_einde_false(Spel.spelers speler_nummer)
         {
+            // Arrange
+            Bord bord = new Bord(6, 1, true);
+            Check_Einde_Spel einde = new Check_Einde_Spel();
+            Speler speler = new Speler(speler_nummer);
 
+            // Act
+            bool is_einde = einde.check_einde(bord, speler);
+
+            // Assert
+            Assert.False(is_einde);
         }
     }
 }
--- mso3/MSO3 Testing/Test_Kuiltje.cs
+++ mso3/MSO3 Testing/Test_Kuiltje.cs
@@ -6,16 +6,74 @@ namespace MSO3_Testing
 {
     public class Test_Kuiltje
     {
-        [Fact]
-        public void gooi_steentje()
+        [Theory]
+        [InlineData(0,1)]
+        [InlineData(5, 6)]
+        [InlineData(-1, 0)]
+        public void gooi_steentje_kuiltje_normaal(int begin_aantal, int expected)
         {
+            // Arrange
+            Kuiltje kuil = new Kuiltje_Normaal(Spel.spelers.p1, begin_aantal);
 
+            // Act
+            kuil.gooi_steentje();
+            int stenen = kuil.steentjes;
+
+            // Assert
+            Assert.Equal(expected, stenen);
         }
 
-        [Fact]
-        public void haal_leeg()
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(5, 6)]
+        [InlineData(-1, 0)]
+        public void gooi_steentje_kuiltje_thuis(int begin_aantal, int expected)
         {
+            // Arrange
+            Kuiltje kuil = new Kuiltje_Thuis(Spel.spelers.p1);
+            kuil.steentjes = begin_aantal;
 
+            // Act
+            kuil.gooi_steentje();
+            int stenen = kuil.steentjes;
+
+            // Assert
+            Assert.Equal(expected, stenen);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(-10)]
+        public void haal_leeg_kuiltje_normaal(int begin_aantal)
+        {
+            // Arrange
+            Kuiltje kuil = new Kuiltje_Normaal(Spel.spelers.p1, begin_aantal);
+
+            // Act
+            kuil.haal_leeg();
+            int stenen = kuil.steentjes;
+
+            // Assert
+            Assert.Equal(0, stenen);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(-10)]
+        public void haal_leeg_kuiltje_thuis(int begin_aantal)
+        {
+            // Arrange
+            Kuiltje kuil = new Kuiltje_Thuis(Spel.spelers.p1);
+            kuil.steentjes = begin_aantal;
+
+            // Act
+            kuil.haal_leeg();
+            int stenen = kuil.steentjes;
+
+            // Assert
+            Assert.Equal(stenen, begin_aantal);
         }
     }
 }
